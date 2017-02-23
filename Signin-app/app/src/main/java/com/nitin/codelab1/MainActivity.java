0a9632c6c921e1f731762344223a032e6b24176e
searce <nitinitleen1@gmail.com>
2017-02-23 17:27:32 +0530

    added behaviour

--- Signin-app/app/src/main/java/com/nitin/codelab1/MainActivity.java
+++ Signin-app/app/src/main/java/com/nitin/codelab1/MainActivity.java
@@ -1,13 +1,49 @@
 package com.nitin.codelab1;
 
+import android.app.PendingIntent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
+public class MainActivity extends FragmentActivity implements ConnectionCallbacks,OnConnectionFailedListener, View.OnClickListener {
+    private static final int SIGNED_IN = 0;
+    private static final int STATE_SIGNING_IN = 1;
+    private static final int STATE_IN_PROGRESS = 2;
+    private static final int RC_SIGN_IN = 0;
+
+    private GoogleApiClient mGoogleApiClient;
+    private int mSignInProgress;
+    private PendingIntent mSignInIntent;
+
+    private SignInButton mSignInButton;
+    private Button mSignOutButton;
+    private Button mRevokeButton;
+    private TextView mStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get references to all of the UI views
+        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        mSignOutButton = (Button) findViewById(R.id.sign_out_button);
+        mRevokeButton = (Button) findViewById(R.id.revoke_access_button);
+        mStatus = (TextView) findViewById(R.id.statuslabel);
+
+        // Add click listeners for the buttons
+        mSignInButton.setOnClickListener(this);
+        mSignOutButton.setOnClickListener(this);
+        mRevokeButton.setOnClickListener(this);
+
+        // Build a GoogleApiClient
+        mGoogleApiClient = buildGoogleApiClient();
     }
 }
